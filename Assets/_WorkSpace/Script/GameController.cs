5a57a915d521ed5921687620f3012e60e08a0f79
SosukeKato <dokindokinpiano@gmail.com>
2026-02-19 22:52:41 +0900

    [update]PlayerActionController内の処理をGameControllerから実行

--- Assets/_WorkSpace/Script/GameController.cs
+++ Assets/_WorkSpace/Script/GameController.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private PlayerActionController playerActionController = new();
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        playerActionController.Tick();
     }
 }
