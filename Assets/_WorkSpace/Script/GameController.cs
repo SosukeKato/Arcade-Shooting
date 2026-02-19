e02a8c71f0d4ccbd3ef89405f3c7b4b9870f5365
SosukeKato <dokindokinpiano@gmail.com>
2026-02-19 23:03:59 +0900

    [update]GameControllerから他のControllerクラスを取得

--- Assets/_WorkSpace/Script/GameController.cs
+++ Assets/_WorkSpace/Script/GameController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     private PlayerActionController playerActionController = new();
+    private AudioController audioController = new();
+    private SceneController sceneController = new();
 
     void Start()
     {
