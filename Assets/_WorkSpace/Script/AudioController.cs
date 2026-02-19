6aea589c37fffe290de31a9e5081797a82d8413f
SosukeKato <dokindokinpiano@gmail.com>
2026-02-19 22:56:36 +0900

    [update]BGMとSEの再生

--- Assets/_WorkSpace/Script/AudioController.cs
+++ Assets/_WorkSpace/Script/AudioController.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioController : MonoBehaviour
+public class AudioController
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayBGM()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySE()
     {
-        
+
     }
 }
