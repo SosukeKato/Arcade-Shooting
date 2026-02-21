655daa81018884af080cf80bfbcf212115840faa
SosukeKato <dokindokinpiano@gmail.com>
2026-02-21 21:40:28 +0900

    [add]EnemyController.cs

--- Assets/_WorkSpace/Script/SceneController.cs
+++ Assets/_WorkSpace/Script/SceneController.cs
@@ -2,6 +2,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneController
 {
+    /// <summary>
+    /// SceneØ‚è‘Ö‚¦
+    /// </summary>
+    /// <param name="SceneName">Scene–¼</param>
     public void LoadScene(string SceneName)
     {
         SceneManager.LoadScene(SceneName);
--- Assets/_WorkSpace/Script/StateController.cs
+++ Assets/_WorkSpace/Script/StateController.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 public class StateController
 {
     public enum GameState
