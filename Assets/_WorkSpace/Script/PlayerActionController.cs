21ae6cbfdafb6a114c95d0eba6a8a8da8f1b9fe6
SosukeKato <dokindokinpiano@gmail.com>
2026-02-21 19:18:07 +0900

    [update]攻撃処理の一部

--- Assets/_WorkSpace/Script/PlayerActionController.cs
+++ Assets/_WorkSpace/Script/PlayerActionController.cs
@@ -27,13 +27,31 @@ public class PlayerActionController : MonoBehaviour
         {
             _isClick = false;
         }
+    }
 
+    /// <summary>
+    /// UŒ‚‚Ìˆ—
+    /// </summary>
+    /// <param name="timer">‚Ç‚ê‚­‚ç‚¢‚ÌŠÔUŒ‚ƒ{ƒ^ƒ“‚ğ‰Ÿ‚µ‚½‚©</param>
+    /// <param name="attackInterval">UŒ‚ƒ{ƒ^ƒ“‚ğ‰Ÿ‚µ‚Ä‚¢‚éŠÔ‚ªUŒ‚‚ÌCT‚ğ’´‚¦‚½‚©</param>
+    public void Attack(float timer, float attackInterval)
+    {
         if (_isClick)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            timer += Time.deltaTime;
+
+            if (timer >= attackInterval)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            }
         }
     }
 
+    /// <summary>
+    /// ƒŠƒ[ƒh‚Ìˆ—
+    /// </summary>
+    /// <param name="magazine">Œ»İ‚Ì‘•’e”</param>
+    /// <param name="magazineMax">Å‘å‘•’e”</param>
     public void Reload(int magazine, int magazineMax)
     {
         if (_reloadGetKeyDown)
