85b37d6592fbf20b421ec2cba0c66cb12bea944c
SosukeKato <dokindokinpiano@gmail.com>
2026-02-26 22:25:36 +0900

    [update]Enemy共通の死亡処理

--- Assets/_WorkSpace/Script/EnemyBase.cs
+++ Assets/_WorkSpace/Script/EnemyBase.cs
@@ -10,32 +10,77 @@ public enum EnemyState
 
 public class EnemyBase : MonoBehaviour
 {
-    [SerializeField] int _currentHP;
+    Animator _anim;
+
+    [SerializeField] int _maxHP;
+    [SerializeField] float _animationTime;
+
+    int _currentHP;
+    float _animationTimer;
+
     EnemyState _currentState;
+
+    /// <summary>
+    /// 生成タイミングで処理する
+    /// </summary>
+    void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// オブジェクトをtrueにしたときに呼び出される(初期化処理)
+    /// </summary>
+    void OnEnable()
+    {
+        _currentHP = _maxHP;
+        _currentState = EnemyState.Idle;
+    }
+
+    /// <summary>
+    /// 子クラスに委託するUpdate処理
+    /// </summary>
+    void Tick()
+    {
+        if (_currentState == EnemyState.Death)
+        {
+            if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Death") && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            {
+                Destroy(gameObject);//後からObjectPoolのReturnObject()に変更する
+            }
+        }
+    }
 
     /// <summary>
     /// 攻撃処理(Stateが攻撃の時のみ呼び出される)
     /// </summary>
-    /// <param name="motionTimer"></param>
-    void Attack(int motionTimer)
+    void Attack()
     {
-        if (_currentState == EnemyState.Attack)
-        {
+        if (_currentState == EnemyState.Attack) return;
 
-        }
+        _anim.SetTrigger("Attack");
+        _currentState = EnemyState.Attack;
     }
 
     /// <summary>
     /// 死亡処理
     /// </summary>
-    /// <param name="motionTimer"></param>
-    void Death(int motionTimer)
+    void Death()
     {
+        if (_currentState == EnemyState.Death) return;
 
+        _anim.SetTrigger("Death");
+        _currentState = EnemyState.Death;
     }
 
-    bool IsAlive()
+    /// <summary>
+    /// HPの処理
+    /// </summary>
+    void Health()
     {
-        return _currentHP > 0;
+        if (_currentHP < 0)
+        {
+            Death();
+        }
     }
 }
