20525934715c8cc2ad733548a598c19dd5727265
SosukeKato <dokindokinpiano@gmail.com>
2026-02-26 22:51:42 +0900

    [update]攻撃処理を追加

--- Assets/_WorkSpace/Script/NormalEnemy.cs
+++ Assets/_WorkSpace/Script/NormalEnemy.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NormalEnemy : MonoBehaviour
+public class NormalEnemy : EnemyBase
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        
+        base.Update();
+    }
+
+    /// <summary>
+    /// 攻撃モーション待機処理(Stateが攻撃の時のみ呼び出される)
+    /// </summary>
+    void Attack()
+    {
+        if (_currentState == EnemyState.Attack) return;
+
+        _anim.SetTrigger("Attack");
+        _currentState = EnemyState.Attack;
     }
 }
